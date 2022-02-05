511dee7eb70aa77fcc15f21e153b5cb45a0903f3
Leonardo Torres <leoes98@gmail.com>
2022-02-05 13:30:59 -0700

    Updated API files

---
+++ api/fast.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# --------------------------
+class TensorflowLiteClassificationModel:
+
+    def __init__(self, model_path, labels, image_size=224):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+        self._input_details = self.interpreter.get_input_details()
+        self._output_details = self.interpreter.get_output_details()
+        self.labels = labels
+        self.image_size = image_size
+
+    def run_from_filepath(self, input_image):
+        input_data_type = self._input_details[0]["dtype"]
+        """
+        image = np.array(input_image.resize(
+            (self.image_size, self.image_size)),
+                         dtype=input_data_type)
+        """
+
+        image = np.array(np.resize(input_image,
+            (self.image_size, self.image_size, 3)),
+                         dtype=input_data_type)
+
+        if input_data_type == np.float32:
+            image = image / 255.
+
+        if image.shape == (1, 224, 224):
+            image = np.stack(image * 3, axis=0)
+
+        if image.shape == (224, 224, 3):
+            image = np.expand_dims(image, axis=0)
+
+        return self.run(image)
+
+    def run(self, image):
+        """
+        args:
+          image: a (1, image_size, image_size, 3) np.array
+
+        Returns list of [Label, Probability], of type List<str, float>
+        """
+
+        self.interpreter.set_tensor(self._input_details[0]["index"], image)
+        self.interpreter.invoke()
+        tflite_interpreter_output = self.interpreter.get_tensor(
+            self._output_details[0]["index"])
+        probabilities = list(tflite_interpreter_output[0])
+
+        # create list of ["label", probability], ordered descending probability
+        label_to_probabilities = []
+        for i, probability in enumerate(probabilities):
+            label_to_probabilities.append([self.labels[i], float(probability)])
+        return sorted(label_to_probabilities, key=lambda element: element[1])
+
+@app.get("/")
+def index():
+    return {"greeting": "Working API"}
+
+@app.get("/predict")
+def predict(img_file: UploadFile = File(...)):
+    model_path = "model_model-export_icn_tflite-dr_deep_model_v2_20220126044745-2022-01-29T21_41_28.325372Z_model.tflite"
+    labels = [1, 3, 2, 0]
+    model = TensorflowLiteClassificationModel(model_path, labels)
+    input_image = Image.open(img_file.file)
+    label_probability = model.run_from_filepath(np.array((input_image)))
+    return label_probability
